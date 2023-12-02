7bfa06a9d7b3413ce6da90990d999d0ed11f4e71
Evgenii Trofimov <veugenet@gmail.com>
2023-12-02 12:02:24 +0300

    axum 7

--- main/src/main.rs
+++ main/src/main.rs
@@ -10,6 +10,7 @@ use futures::stream::StreamExt;
 use futures_util::SinkExt;
 use futures_util::stream::SplitStream;
 use tokio::time::timeout;
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
 use tracing::Instrument;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -26,13 +27,17 @@ async fn main() {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "tower_http=debug".into()),
+                .unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
 
     let app = Router::new()
-        .route("/ws", get(ws_handler));
+        .route("/ws", get(ws_handler))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        );;
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
