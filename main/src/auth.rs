854ec3976159b07ca3d6c7bd0d561b550d369f01
Evgenii Trofimov <veugenet@gmail.com>
2024-05-02 09:44:56 +0300

    tracing

--- main/src/auth.rs
+++ main/src/auth.rs
@@ -1,14 +1,30 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use axum::{
-    extract::{
-        Query, Request
-
+    async_trait, extract::{
+        FromRef, FromRequestParts, Query, Request
     }, 
-    middleware::Next, 
-    response::{IntoResponse, Response},
+    http::{request::Parts, StatusCode}, middleware::Next, response::{IntoResponse, Response}, 
+    Json, RequestPartsExt
 };
+use casdoor_rust_sdk::{AuthService, CasdoorConfig, CasdoorUser};
+use axum_extra::{
+    headers::{authorization::Bearer, Authorization},
+    TypedHeader,
+};
+use serde_json::json;
+use once_cell::sync::Lazy;
+use tower_http::trace::TraceLayer;
+use std::{borrow::Borrow, env::{self, VarError}};
+use std::fmt::Display;
 
-
+static CONF: Lazy<CasdoorConfig> = Lazy::new(|| {
+    let url = env::var("CASDOOR_URL")
+        .unwrap_or_else(|_| "http://localhost:8000".to_owned());
+    tracing::debug!("test");
+    CasdoorConfig::new(url, 
+        "".to_owned(), "".to_owned(), 
+        "".to_owned(), "".to_owned(), Some("".to_owned()))
+});
 
 #[derive(Deserialize)]
 struct AuthQuery {
@@ -19,4 +35,75 @@ pub async fn ws_auth(request: Request, next: Next) -> Result<impl IntoResponse, 
     let token: Query<AuthQuery> = Query::try_from_uri(request.uri()).unwrap();
 
     Ok(next.run(request).await)
+}
+
+pub struct AuthState<'a> {
+    service: AuthService<'a>
+}
+
+impl<'a> Clone for AuthState<'a> {
+    fn clone(&self) -> Self {
+        Self { service: AuthService::new(&CONF) }
+    }
+}
+
+impl<'a> AuthState<'a> {
+    pub fn new() -> Self {
+        Self {
+            service: AuthService::new(&CONF)
+        }
+    }
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Claims
+where
+    S: Send + Sync
+{
+    type Rejection = AuthError;
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| AuthError::InvalidToken)?;
+        
+        Ok(Claims{
+            id: "".to_owned()
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum AuthError {
+    WrongCredentials,
+    MissingCredentials,
+    TokenCreation,
+    InvalidToken,
+}
+
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "Wrong credentials"),
+            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
+            AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+        };
+        let body = Json(json!({
+            "error": error_message,
+        }));
+        (status, body).into_response()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    id: String
+}
+
+impl Display for Claims {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "id: {}\n", self.id)
+    }
 }
--- main/src/main.rs
+++ main/src/main.rs
@@ -1,10 +1,12 @@
+use auth::AuthState;
 use axum::{
     Router, routing::get,
-    middleware::{self}, Json
+    middleware::{self},
+    extract::State
 };
+use casdoor_rust_sdk::CasdoorConfig;
 use std::net::SocketAddr;
 
-use tower_http::trace::{DefaultMakeSpan, TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod client;
@@ -13,18 +15,35 @@ mod ws;
 
 #[tokio::main]
 async fn main() {
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "example_websockets=debug,tower_http=debug".into()),
+                .unwrap_or_else(|_| "main=debug,tower_http=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new()
+    if let Err(_) = dotenvy::dotenv() {
+
+    }
+    
+    let ws = Router::new()
         .route("/ws", get(ws::ws_handler))
         .layer(middleware::from_fn(auth::ws_auth));
+    let restricted = Router::new()
+        .route("/rs", get(restricted))
+        .with_state(AuthState::new());
+    let app = Router::new()
+        .nest("", ws)
+        .nest("", restricted);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
+}
+
+async fn restricted(claims: auth::Claims) -> Result<String, auth::AuthError> {
+    Ok(format!(
+        "Welcome to the protected area :)\nYour data:\n{claims}",
+    ))
 }
